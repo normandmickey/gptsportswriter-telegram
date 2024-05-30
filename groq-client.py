ec834eb5bb08048a35dd26bbf216850c5919a90f
normandmickey <normandmickey@gmail.com>
2024-05-30 07:38:15 -0400

    Update groq-client.py

--- groq-client.py
+++ groq-client.py
@@ -10,10 +10,8 @@ tavily = TavilyClient(api_key=os.environ.get("TAVILY_API_KEY"))
 
 def request_groq(user_message):
     try:
-        print(user_message)
         response = tavily.search(query=user_message, search_depth="advanced")
         context = [{"body": obj["content"]} for obj in response.get("results", [])]
-        print(context)
         completion = client.chat.completions.create(
             model="llama3-70b-8192",
             messages=[
@@ -21,7 +19,6 @@ def request_groq(user_message):
                 {"role": "user", "content": str(context) + user_message}
             ]
         )
-        #print(completion.choices[0].message.content)
         return completion.choices[0].message.content
     except Exception as e:
         print(f"An error occurred: {e}")
